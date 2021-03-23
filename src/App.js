b11c23b04c2209b2e140fe4b4192d24b3695fb0b
Justyna <jmwajman@gmail.com>
2021-03-23 21:24:43 +0100

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Dictionary
+        </h1>
+      <footer><a href="https://github.com/jmwajman"> Open source </a> code by JM Wajman</footer>
+    </div>
+  );
+}
+
+export default App;
